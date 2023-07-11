8f62821735c5403bf6dafd7174c09581e82e680d
lyntchang <irenedielynsha@yahoo.com>
2023-07-11 09:26:00 -0400

    fake shop app

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import Header from './containers/Header';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ProductListing from './containers/ProductListing';
+import ProductDetails from './containers/ProductDetails';
+
+
+function App() {
+  return (
+    <div className='App'>
+      <Router>
+        <Header/>
+        <Routes>
+          <Route path = '/' exact element = {<ProductListing/>}/>
+          <Route path = '/product/:productId' exact element = {<ProductDetails/>} /> 
+          <Route>404 Not Found!</Route>
+        </Routes> 
+
+      </Router>
+     
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/containers/ProductListing.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import ProductComponent from "./ProductComponent";
+import axios from "axios";
+import { setProducts } from "../redux/actions/productActions";
+
+import { useEffect } from "react";
+
+const ProductListing = () =>{
+    const products = useSelector(state => state);
+    const dispatch = useDispatch()
+    console.log('Products', products);
+
+    const fetchProducts = async() => {
+        const response = await axios
+        .get('https://fakestoreapi.com/products')
+        .catch((err) =>{
+            console.log('Err',err);
+        });
+        dispatch(setProducts(response.data));
+    };
+    useEffect(() =>{
+        fetchProducts();
+    }, []);
+    return(
+        <div className="ui grid container">
+          <ProductComponent/>
+        </div>
+    )
+}
+
+export default ProductListing;
